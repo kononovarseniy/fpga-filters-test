92f1479317a219c885ba9a45a30e9fcbc2c51245
Arseniy Kononov <kononovarseniy@gmail.com>
2021-12-24 11:57:30 +0700

    Initial commit

---
+++ Filters.vhd
@@ -0,0 +1,18 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Filters is
+	port (
+		clk_i: in std_logic;
+		signal_i: in integer;
+		signa_o: out integer
+	);
+end entity;
+
+architecture rtl of Filters is
+
+begin
+
+end architecture;
